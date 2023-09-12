508d9b937571d335587372324dc6787abff55e41
hachicombu <frefg25@gmail.com>
2023-09-12 09:22:54 +0900

    日付をutcからjstに変更

--- src/library/common.ts
+++ src/library/common.ts
@@ -1,10 +1,14 @@
 import { Blog } from "./microcms";
+// 日付をUTC -> JSTに変換
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 // 日付のフォーマット変更
 export const setDateFormat = (blog: Blog) => {
-  return new Date(blog.publishedAt).toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
+  const publishedDate = dayjs.utc(blog.publishedAt).tz("Asia/Tokyo").format("YYYY/MM/DD");
+
+  return publishedDate;
 };
