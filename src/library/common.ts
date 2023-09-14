1f0e4847a25308f152430df03b758c07291d8d23
hachicombu <frefg25@gmail.com>
2023-09-15 02:11:59 +0900

    日付処理の書き方修正

--- src/library/common.ts
+++ src/library/common.ts
@@ -3,12 +3,13 @@ import { Blog } from "./microcms";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Tokyo");
 
 // 日付のフォーマット変更
 export const setDateFormat = (blog: Blog) => {
-  const publishedDate = dayjs.utc(blog.publishedAt).tz("Asia/Tokyo").format("YYYY/MM/DD");
-
+  const publishedDate = dayjs(blog.publishedAt).tz().format("YYYY/MM/DD");
   return publishedDate;
 };
